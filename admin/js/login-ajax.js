d388541cf65285f7af1ca3bdf940a8b576b5698f
marianalG0 <garciahaumariana@gmail.com>
2022-06-13 20:32:31 -0500

    Revisando

--- admin/js/login-ajax.js
+++ admin/js/login-ajax.js
@@ -1,37 +1,36 @@
 $(document).ready(function(){
 
 
-$('#login-admin').on('submit', function(e){
-    e.preventDefault();
+    $('#login-admin').on('submit', function(e) {
+        e.preventDefault();
 
-    var datos = $(this).serializeArray();
+        var datos = $(this).serializeArray();
 
-    //Creamos el llamado ajax
-    $.ajax({
-        type: $(this).attr('method'),
-        data: datos,
-        url: $(this).attr('action'),
-        dataType: 'json',
-        success: function(data){
-            var resultado = data;
-            if(resultado.respuesta == 'exitoso'){
-                swal(
-                    'Login Correcto!',
-                    'Bienvenid@'+resultado.usuario+ ' !! ',
-                    'success'
-                  )
-                  setTimeout(function(){
-                    window.location.href = 'admin-area.php';
-                  }, 2000);
-            }else{
-                swal(
-                    'Error',
-                    'Usuario o Password Incorrectos',
-                    'error'
-                  )
+        $.ajax({
+            type: $(this).attr('method'),
+            data: datos, 
+            url: $(this).attr('action'),
+            dataType: 'json',
+            success: function(data){
+                console.log(data);
+                var resultado = data;
+                if(resultado.respuesta == 'exitoso') {
+                    swal(
+                        'Login Correcto',
+                        'Bienvenid@ '+resultado.usuario+' !! ',
+                        'success'
+                    )
+                    setTimeout(function(){
+                        window.location.href = 'admin-area.php';
+                    }, 2000);
+                } else {
+                    swal(
+                        'Error!',
+                        'Usuario o password Incorrectos',
+                        'error'
+                    )
+                }
             }
-        }
-    })
+        })
+    });
 });
-
-});w
